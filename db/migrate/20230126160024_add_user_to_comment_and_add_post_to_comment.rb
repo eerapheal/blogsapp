a2580d58ae26cc7583795980e7f47806f33a07b1
unknown <ekpenisiraphael@gmail.com>
2023-01-27 02:47:52 +0530

    Model generated dynamically and Migreated to db

--- db/migrate/20230126160024_add_user_to_comment_and_add_post_to_comment.rb
+++
@@ -1,7 +0,0 @@
-class AddUserToCommentAndAddPostToComment < ActiveRecord::Migration[7.0]
-
-  add_reference :comments, :author, null: false, foreign_key: { to_table: :users }
-    add_reference :comments, :posts, null: false, foreign_key: true
-  def change
-  end
-end
