c4cb299e4370746db63a7fa325c0d3cb4159e33a
unknown <ekpenisiraphael@gmail.com>
2023-01-27 00:44:01 +0530

    asociation of tables implemented

--- db/migrate/20230126155647_create_comment.rb
+++ db/migrate/20230126155647_create_comment.rb
@@ -3,6 +3,8 @@ class CreateComment < ActiveRecord::Migration[7.0]
     create_table :comments do |t|
      
       t.text :text
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.references :post, null: false, foreign_key: true
       t.timestamps
     end
   end
