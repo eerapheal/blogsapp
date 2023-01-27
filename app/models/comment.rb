10ebf45d52d1d20b5f6f698e3c7fcde001ab3a63
unknown <ekpenisiraphael@gmail.com>
2023-01-27 12:24:33 +0530

    Linter checked

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-    belongs_to :post
+  belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   def update_comments_counter
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
-
-    has_many :likes, foreign_key: 'posts_id'
+  has_many :likes, foreign_key: 'posts_id'
   has_many :comments, foreign_key: 'posts_id'
   belongs_to :author, class_name: 'User', dependent: :destroy, foreign_key: 'author_id'
 end
