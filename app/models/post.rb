a679fd785041cd9596a78475c35378aeff9842fb
unknown <ekpenisiraphael@gmail.com>
2023-01-28 03:53:31 +0530

    Bug fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,6 @@ class Post < ApplicationRecord
   validates :author_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-
   def last_five_comments
     comments.last(5)
   end
