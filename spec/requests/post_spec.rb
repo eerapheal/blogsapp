a4301b9e3c389ac4d598e50befce0f76f7a49b7b
unknown <ekpenisiraphael@gmail.com>
2023-01-31 02:21:56 +0530

    Linters checked

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/post/show"
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/post/show'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
