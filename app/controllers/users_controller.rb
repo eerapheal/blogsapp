0ba7ba8d365c47887912f9fbae112fe7f31c271d
unknown <ekpenisiraphael@gmail.com>
2023-02-03 18:47:44 +0530

    Linters checked

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-    def index
-      @users = User.all
-    end
-  
-    def show
-      user_id = params[:id].to_i
-      @user = User.find(user_id)
-      @posts = @user.recent_posts
-    end
+  def index
+    @users = User.all
+  end
+
+  def show
+    user_id = params[:id].to_i
+    @user = User.find(user_id)
+    @posts = @user.recent_posts
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,21 @@
 class Post < ApplicationRecord
-    validates :title, presence: true
-    validates :title, length: { maximum: 250 }
-    validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
-    validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
-  
-    after_create :update_posts_counter
-  
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-    has_many :likes
-    has_many :comments
-  
-    def update_posts_counter
-      author_id = author.id
-      User.increment_counter(:posts_counter, author_id)
-    end
-  
-    def recent_comments
-      comments.order(created_at: :desc).limit(5)
-    end
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
+
+  after_create :update_posts_counter
+
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :likes
+  has_many :comments
+
+  def update_posts_counter
+    author_id = author.id
+    User.increment_counter(:posts_counter, author_id)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
