4f6ffd9505f645d96d8ac3fe190a1e3be569be2d
unknown <ekpenisiraphael@gmail.com>
2023-02-03 17:22:14 +0530

    user view implemented using patial template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
-  # GET /users/:user_id/posts
   def index
-    @posts = Post.all
+    user_id = params[:user_id]
+    @user = User.find(user_id)
+    @posts = Post.where(author_id: user_id)
   end
 
-  # GET /users/:user_id/posts/:id
   def show
-    params[:id]
+    user_id = params[:user_id]
+    post_id = params[:id]
+    @user = User.find(user_id)
+    @post = Post.find(post_id)
+    @comments = Comment.where(post_id:)
   end
 end
